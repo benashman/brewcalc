cfe12970a91531e0114e3805650a91ea1aa5a28a
Benjamin Ashman <hello@benashman.me>
2020-07-02 11:20:35 -0700

    Fixed value bindings

--- Brewcalc/ContentView.swift
+++ Brewcalc/ContentView.swift
@@ -9,129 +9,232 @@
 import SwiftUI
 import Foundation
 
-struct ContentView: View {
+enum Components {
+    static let coffee = "coffee"
+    static let ratio = "ratio"
+    static let water = "water"
+}
 
-    @State var coffeeAmount: Double = UserDefaults.standard.double(forKey: "coffeeAmount")
-    @State var waterAmount: Double = UserDefaults.standard.double(forKey: "waterAmount")
-    @State var waterRatio: Double = UserDefaults.standard.double(forKey: "waterRatio")
-    
-    @State var isDragging = false
-    @State var offset = CGSize.zero
-    
-    var drag: some Gesture {
-        DragGesture(minimumDistance: 0, coordinateSpace: .global)
-            .onChanged { gesture in
-                self.isDragging = true
-                print("WE ARE DRAGGING")
-            }
-            
-            .onEnded { _ in
-                self.isDragging = false
-            }
-    }
+struct ContentView: View {
+    @AppStorage(Components.coffee) var coffee: Int = 21
+    @AppStorage(Components.ratio) var ratio: Int = 16
+    @AppStorage(Components.water) var water: Int = 336
     
     var body: some View {
         
-        let ratioBinding = Binding(
-            get: { UserDefaults.standard.double(forKey: "waterRatio") },
-            set: {
-                self.waterRatio = $0
-                UserDefaults.standard.set(self.waterRatio, forKey: "waterRatio")
-                
-                self.waterAmount = $0 * self.coffeeAmount
-                UserDefaults.standard.set(self.waterAmount, forKey: "waterAmount")
+        let coffeeBinding: Binding<Int> = Binding(
+            get: { coffee },
+            set: { val in
+                coffee = val
+                water = val * ratio
             }
         )
         
-        let coffeeBinding = Binding(
-            get: { UserDefaults.standard.double(forKey: "coffeeAmount") },
-            set: {
-                self.coffeeAmount = $0
-                UserDefaults.standard.set(self.coffeeAmount, forKey: "coffeeAmount")
-                
-                self.waterAmount = $0 * self.waterRatio
-                UserDefaults.standard.set(self.waterAmount, forKey: "waterAmount")
+        let ratioBinding: Binding<Int> = Binding(
+            get: { ratio },
+            set: { val in
+                ratio = val
+                water = val * coffee
             }
         )
         
-        let waterBinding = Binding(
-            get: { UserDefaults.standard.double(forKey: "waterAmount") },
-            set: {
-                self.waterAmount = $0
-                UserDefaults.standard.set(self.waterAmount, forKey: "waterAmount")
-                
-                self.coffeeAmount = $0 / self.waterRatio
-                UserDefaults.standard.set(self.coffeeAmount, forKey: "coffeeAmount")
+        let waterBinding: Binding<Int> = Binding(
+            get: { water },
+            set: { val in
+                water = val
+                coffee = val / ratio
             }
         )
         
-        return VStack( alignment: .center, spacing: 64) {
-            
-            // Coffee amount
-            VStack(spacing: 4) {
+        return VStack {
+            HStack {
                 Text("Coffee")
-                    .font(.system(size: 16, weight: .regular, design: .monospaced))
-                
-                Nudger(value: coffeeBinding, range: 1...100) {
-                    Text("\(Int(coffeeAmount))g")
-                        .font(.system(size: 56, weight: .regular, design: .monospaced))
+                Nudger(value: coffeeBinding) {
+                    Text("\(self.coffee)g")
                 }
             }
-            
-            // Coffee:Water Ratio
-            VStack(spacing: 4) {
-                Text("Coffee:Water")
-                    .font(.system(size: 16, weight: .regular, design: .monospaced))
-                
-                Nudger(value: ratioBinding, range: 1...30) {
-                    Text("1:\(Int(waterRatio))")
-                        .font(.system(size: 56, weight: .regular, design: .monospaced))
+
+            HStack {
+                Text("Ratio")
+                Nudger(value: ratioBinding) {
+                    Text("1:\(self.ratio)")
                 }
             }
-            
-            // Water amount
-            VStack(spacing: 4) {
+
+            HStack {
                 Text("Water")
-                    .font(.system(size: 16, weight: .regular, design: .monospaced))
-                
-                Nudger(value: waterBinding, range: 1...1000) {
-                    Text("\(Int(waterAmount))g")
-                        .font(.system(size: 56, weight: .regular, design: .monospaced))
+                Nudger(value: waterBinding) {
+                    Text("\(self.water)g")
                 }
             }
-        }.padding()
+        }
     }
 }
 
 struct Nudger<Content: View>: View {
-    @Binding var value: Double
-    @State var range: ClosedRange<CGFloat>
+    @Binding var value: Int
+    @State var previousValue: Int = 0
     
+    @State var dragValue = CGSize.zero
     @State var isDragging = false
-    @GestureState var dragAmount = CGSize.zero
+    
+    var calculatedValue: Int {
+        Int(dragValue.width) / 20
+    }
     
     let content: () -> Content
-
+    
     var body: some View {
-        Group {
-            Text("\(Int(self.value + Double((dragAmount.width))))")
-            content()
-                .foregroundColor(self.isDragging ? Color.yellow : Color.primary)
-                .offset(dragAmount)
-                .gesture(
-                    DragGesture()
-                        .updating($dragAmount) { v, state, transaction in
-                            withAnimation(.spring()) {
-                                state.width = v.translation.width/20
-                            }
-                        }
-                        .onChanged({ (v) in
-                            self.value += round(Double(dragAmount.width/20))
-                        })
-                )
-        }
+        content()
+            .gesture(
+                DragGesture()
+                    .onChanged { v in
+                        dragValue.width = v.translation.width
+                        self.value = previousValue + Int(v.translation.width/20)
+                    }
+                    .onEnded { _ in
+                        previousValue = self.value
+                    }
+            )
+            .onAppear {
+                self.previousValue = self.value
+            }
     }
 }
+
+//    @Binding var value: Double
+//    @State var range: ClosedRange<CGFloat>
+//
+//    @State var isDragging = false
+//    @GestureState var dragAmount = CGSize.zero
+//
+//    let content: () -> Content
+//
+
+//struct ContentView: View {
+//
+//    @State var coffeeAmount: Double = UserDefaults.standard.double(forKey: "coffeeAmount")
+//    @State var waterAmount: Double = UserDefaults.standard.double(forKey: "waterAmount")
+//    @State var waterRatio: Double = UserDefaults.standard.double(forKey: "waterRatio")
+//
+//    @State var isDragging = false
+//    @State var offset = CGSize.zero
+//
+//    var drag: some Gesture {
+//        DragGesture(minimumDistance: 0, coordinateSpace: .global)
+//            .onChanged { gesture in
+//                self.isDragging = true
+//                print("WE ARE DRAGGING")
+//            }
+//
+//            .onEnded { _ in
+//                self.isDragging = false
+//            }
+//    }
+//
+//    var body: some View {
+//
+//        let ratioBinding = Binding(
+//            get: { UserDefaults.standard.double(forKey: "waterRatio") },
+//            set: {
+//                self.waterRatio = $0
+//                UserDefaults.standard.set(self.waterRatio, forKey: "waterRatio")
+//
+//                self.waterAmount = $0 * self.coffeeAmount
+//                UserDefaults.standard.set(self.waterAmount, forKey: "waterAmount")
+//            }
+//        )
+//
+//        let coffeeBinding = Binding(
+//            get: { UserDefaults.standard.double(forKey: "coffeeAmount") },
+//            set: {
+//                self.coffeeAmount = $0
+//                UserDefaults.standard.set(self.coffeeAmount, forKey: "coffeeAmount")
+//
+//                self.waterAmount = $0 * self.waterRatio
+//                UserDefaults.standard.set(self.waterAmount, forKey: "waterAmount")
+//            }
+//        )
+//
+//        let waterBinding = Binding(
+//            get: { UserDefaults.standard.double(forKey: "waterAmount") },
+//            set: {
+//                self.waterAmount = $0
+//                UserDefaults.standard.set(self.waterAmount, forKey: "waterAmount")
+//
+//                self.coffeeAmount = $0 / self.waterRatio
+//                UserDefaults.standard.set(self.coffeeAmount, forKey: "coffeeAmount")
+//            }
+//        )
+//
+//        return VStack( alignment: .center, spacing: 64) {
+//
+//            // Coffee amount
+//            VStack(spacing: 4) {
+//                Text("Coffee")
+//                    .font(.system(size: 16, weight: .regular, design: .monospaced))
+//
+//                Nudger(value: coffeeBinding, range: 1...100) {
+//                    Text("\(Int(coffeeAmount))g")
+//                        .font(.system(size: 56, weight: .regular, design: .monospaced))
+//                }
+//            }
+//
+//            // Coffee:Water Ratio
+//            VStack(spacing: 4) {
+//                Text("Coffee:Water")
+//                    .font(.system(size: 16, weight: .regular, design: .monospaced))
+//
+//                Nudger(value: ratioBinding, range: 1...30) {
+//                    Text("1:\(Int(waterRatio))")
+//                        .font(.system(size: 56, weight: .regular, design: .monospaced))
+//                }
+//            }
+//
+//            // Water amount
+//            VStack(spacing: 4) {
+//                Text("Water")
+//                    .font(.system(size: 16, weight: .regular, design: .monospaced))
+//
+//                Nudger(value: waterBinding, range: 1...1000) {
+//                    Text("\(Int(waterAmount))g")
+//                        .font(.system(size: 56, weight: .regular, design: .monospaced))
+//                }
+//            }
+//        }.padding()
+//    }
+//}
+//
+//struct Nudger<Content: View>: View {
+//    @Binding var value: Double
+//    @State var range: ClosedRange<CGFloat>
+//
+//    @State var isDragging = false
+//    @GestureState var dragAmount = CGSize.zero
+//
+//    let content: () -> Content
+//
+//    var body: some View {
+//        Group {
+//            Text("\(Int(self.value + Double((dragAmount.width))))")
+//            content()
+//                .foregroundColor(self.isDragging ? Color.yellow : Color.primary)
+//                .offset(dragAmount)
+//                .gesture(
+//                    DragGesture()
+//                        .updating($dragAmount) { v, state, transaction in
+//                            withAnimation(.spring()) {
+//                                state.width = v.translation.width/20
+//                            }
+//                        }
+//                        .onChanged({ (v) in
+//                            self.value += round(Double(dragAmount.width/20))
+//                        })
+//                )
+//        }
+//    }
+//}
+
 
 
 struct ContentView_Previews: PreviewProvider {
